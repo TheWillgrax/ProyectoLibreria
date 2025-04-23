be7d229f9526c0c3b003e3e78a9bc2ac0ac9a465
TheWillgrax <145234634+TheWillgrax@users.noreply.github.com>
2025-04-22 21:51:31 -0600

    Conexion con SQL Arreglado Prueba dos

---
+++ src/main/java/com/tulibreria/libreria/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.tulibreria.libreria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/productos";  // Cambia a donde tengas tu CRUD (ej: /productos o /proveedores)
+    }
+}
